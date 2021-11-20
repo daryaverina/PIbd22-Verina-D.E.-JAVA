fe1770a01600a355ae2031b5f94e52334f1bbcf9
Daria Verina <dashka400g@gmail.com>
2021-11-20 15:20:39 +0400

    1221

---
+++ src/IlluminatorCircle.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+public class IlluminatorCircle implements IIlluminator{
+    private IlluminatorCount countWin;
+
+    public IlluminatorCircle(int number) {
+        setDigit(number);
+    }
+
+    @Override
+    public void setDigit(int number) {
+        this.countWin = IlluminatorCount.getCount(number);
+    }
+
+    @Override
+    public void draw(Graphics g, int _startPosX, int _startPosY, int _airbusWidth, int _airbusHeight) {
+
+        int x = _startPosX + 70;
+        for (int i = 0; i < 10; i++) {
+            g.setColor(Color.WHITE);
+            g.fillOval(x, _startPosY + 81, 7, 7);
+            g.setColor(Color.BLACK);
+            g.drawOval(x, _startPosY + 81, 7, 7);
+            x += 18;
+        }
+        x = _startPosX + 60;
+        if (countWin == IlluminatorCount.twenty || countWin == IlluminatorCount.thirty) {
+            for (int i = 0; i < 10; i++) {
+                g.setColor(Color.WHITE);
+                g.fillOval(x, _startPosY + 71, 7, 7);
+                g.setColor(Color.BLACK);
+                g.drawOval(x, _startPosY + 71, 7, 7);
+                x += 18;
+            }
+        }
+        x = _startPosX + 50;
+        if (countWin == IlluminatorCount.thirty) {
+            for (int i = 0; i < 10; i++) {
+                g.setColor(Color.WHITE);
+                g.fillOval(x, _startPosY + 61, 7, 7);
+                g.setColor(Color.BLACK);
+                g.drawOval(x, _startPosY + 61, 7, 7);
+                x += 18;
+            }
+        }
+    }
+
+}
+
---
+++ src/IlluminatorSquare.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+public class IlluminatorSquare implements IIlluminator {
+    private IlluminatorCount countWin;
+
+    public IlluminatorSquare(int number) {
+        setDigit(number);
+    }
+
+    @Override
+    public void setDigit(int number) {
+        this.countWin = IlluminatorCount.getCount(number);
+    }
+
+    @Override
+    public void draw(Graphics g, int _startPosX, int _startPosY, int _airbusWidth, int _airbusHeight) {
+
+        int x = _startPosX + 70;
+        for (int i = 0; i < 10; i++) {
+            g.setColor(Color.WHITE);
+            g.fillRect(x,_startPosY+81,5,7);
+            //  g.fillOval(x, _startPosY + 81, 5, 7);
+            g.setColor(Color.BLACK);
+            // g.drawOval(x, _startPosY + 81, 5, 7);
+            g.drawRect(x,_startPosY+81,5,7);
+            x += 18;
+        }
+        x = _startPosX + 60;
+        if (countWin == IlluminatorCount.twenty || countWin == IlluminatorCount.thirty) {
+            for (int i = 0; i < 10; i++) {
+                g.setColor(Color.WHITE);
+                g.fillRect(x,_startPosY+71,5,7);
+                //  g.fillOval(x, _startPosY + 71, 5, 7);
+                g.setColor(Color.BLACK);
+                //g.drawOval(x, _startPosY + 71, 5, 7);
+                g.drawRect(x,_startPosY+71,5,7);
+                x += 18;
+            }
+        }
+        x = _startPosX + 50;
+        if (countWin == IlluminatorCount.thirty) {
+            for (int i = 0; i < 10; i++) {
+                g.setColor(Color.WHITE);
+                //  g.fillOval(x, _startPosY + 61, 5, 7);
+                g.fillRect(x,_startPosY+61,5,7);
+                g.setColor(Color.BLACK);
+                // g.drawOval(x, _startPosY + 61, 5, 7);
+                g.drawRect(x,_startPosY+61,5,7);
+                x += 18;
+            }
+        }
+    }
+}
